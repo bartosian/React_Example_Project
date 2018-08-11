744d1a1007998a8019cb695bfccebd70d7e34cc3
Kiryl Bartashevich <bartosian1989@gmail.com>
2018-08-11 15:19:45 -0400

    React state

--- src/App.js
+++ src/App.js
@@ -3,13 +3,27 @@ import './App.css';
 import Person from "./Person/Person";
 
 class App extends Component {
+
+  state = {
+    people: [
+      {name:"Kiryl"},
+      {name:"Petr"},
+      {name:"Alex"}
+    ]
+  }
+
+  switchName = () => {
+    console.log(this.state.people);
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hi world!</h1>
-        <Person name="Kiryl"/>
-        <Person name="Petr">I like this world!</Person>
-        <Person name="Alex"/>
+        <button onClick={this.switchName}>Click me</button>
+        <Person name={this.state.people[0].name}/>
+        <Person name={this.state.people[1].name}>I like this world!</Person>
+        <Person name={this.state.people[2].name}/>
       </div>
     ); 
 
